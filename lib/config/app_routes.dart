6f9fea4fe787b0037b83429fd377b5838d717dfc
Luan-Cavalcante <luanv.cavalcante@gmail.com>
2023-01-20 11:08:42 -0300

    Adicionando rota de cadastro nas rotas do app.
    Co-Authored-By: João Durso <jvsdurso>
    Co-Authored-By: Vitor Kuhl <vitorekr>
    Co-Authored-By: Nicolas Roberto <Nicolas-Roberto>

--- lib/config/app_routes.dart
+++ lib/config/app_routes.dart
@@ -1,11 +1,14 @@
 import 'package:get/get.dart';
 import 'package:idotpet/modules/auth/ui/login_page.dart';
+import 'package:idotpet/modules/auth/ui/signup_page.dart';
 import 'package:idotpet/modules/home/ui/home_page.dart';
 
 class AppRoutes {
   static const String _home = '/';
   static const String _splash = '/splash';
   static const String _login = '/login';
+  static const String _signup = '/signup';
+
 
   static final pages = [
     GetPage(
@@ -13,13 +16,18 @@ class AppRoutes {
       page: () => const HomePage(),
     ),
     GetPage(
-      name: _splash,
+      name: _login,
       page: () => const LoginPage(),
+    ),
+    GetPage(
+      name: _signup,
+      page: () => const SignUpPage(),
     ),
     
   ];
 
   static String homePath() => '/';
   static String loginPath() => _login;
+  static String signupPath() => _signup;
   static String splashPath() => _splash;
 }
