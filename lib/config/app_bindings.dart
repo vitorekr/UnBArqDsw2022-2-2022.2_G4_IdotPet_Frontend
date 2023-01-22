3d68fae7c03da33b005d32cfb5d174e2b18d9308
Vitor Kühl <vitor.kuhl@iatec.com>
2023-01-22 12:39:36 -0300

    criação app_bindings
    Co-authored-by: Luan-Cavalcante luanv.cavalcante@gmail.com
    Co-authored-by: jvsdurso jvsdurso@gmail.com

---
+++ lib/config/app_bindings.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+import 'package:idotpet/modules/auth/controllers/session_controller.dart';
+import 'package:idotpet/modules/auth/controllers/user_controller.dart';
+import 'package:idotpet/modules/auth/data_provider/iuser_api.dart';
+import 'package:idotpet/modules/auth/data_provider/user_api.dart';
+import 'package:idotpet/modules/auth/usecases/get_user_usecase.dart';
+
+class AppBindings extends Bindings {
+  @override
+  void dependencies() {
+    Get.put<GetUserUseCase>(GetUserUseCase());
+    Get.put<IUserApi>(UserApi());
+    Get.put<UserController>(UserController());
+    Get.put<SessionController>(SessionController());
+
+  }
+}
