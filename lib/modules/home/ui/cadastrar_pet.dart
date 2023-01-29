07a03ff88cc6aadd9c9f65bb7ee8cf16474dc8bc
jvsdurso <69814362+jvsdurso@users.noreply.github.com>
2023-01-28 21:07:49 -0300

    Adicionando Porte e Vacinas
    Co-Authored-By: Luan Cavalcante <67024690+Luan-Cavalcante@users.noreply.github.com>
    Co-Authored-By: vitorekr <56610229+vitorekr@users.noreply.github.com>
    Co-Authored-By: Nícolas Roberto <66215835+Nicolas-Roberto@users.noreply.github.com>

--- lib/modules/home/ui/cadastrar_pet.dart
+++ lib/modules/home/ui/cadastrar_pet.dart
@@ -34,12 +34,101 @@ Widget buildName(){
                   decoration: InputDecoration(
                     contentPadding: EdgeInsets.only(top:14),
                     border: InputBorder.none,
-                    icon: Icon(Icons.person),
-                    labelText: 'Nome',
+                    labelText: 'Ex.: Snoopy',
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
                     labelStyle: TextStyle(
                     color: Colors.black38,
                     fontWeight: FontWeight.w400,
                     fontSize: 20,
+                  ),
+                ),
+           ),
+        ),
+    ],
+  );
+}
+
+Widget buildSize(){
+  return Column(
+    children: <Widget>[
+      Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.only(
+                left: 20,
+              ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              // ignore: prefer_const_literals_to_create_immutables
+              boxShadow: [
+                const BoxShadow(
+                  color: Colors.pink,
+                  blurRadius: 4,
+                  offset: Offset(0,2),
+                )
+              ]
+            ),
+            height: 60,
+            child: const TextField(
+                  keyboardType: TextInputType.emailAddress, // E ESSE EMAILADRESS PRA TODO LADO?
+                  style: TextStyle(
+                    color: Colors.black38,
+                  ),
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.only(top:14),
+                    border: InputBorder.none,
+                    labelText: 'Ex.: Médio',
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                    labelStyle: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
+                  ),
+                ),
+           ),
+        ),
+    ],
+  );
+}
+
+Widget buildVaccines(){
+  return Column(
+    children: <Widget>[
+      Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.only(
+                left: 20,
+              ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              // ignore: prefer_const_literals_to_create_immutables
+              boxShadow: [
+                const BoxShadow(
+                  color: Colors.pink,
+                  blurRadius: 4,
+                  offset: Offset(0,2),
+                )
+              ]
+            ),
+            height: 60,
+            child: const TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(
+                    color: Colors.black38,
+                  ),
+                  maxLines: 5,
+                  minLines: 1,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.only(top:30),
+                    border: InputBorder.none,
+                    labelText: 'Descreva quais vacinas o animal já tomou. \n\nEx.: 4 V8s, 2 de Giardia, 1 de Raiva.',
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                    labelStyle: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
+                    
                   ),
                 ),
            ),
@@ -58,24 +147,41 @@ class CadastrarPet extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromRGBO(233, 0, 84, 1),
-        title: const Text('Inserir Anúncio')
+        title: const Text('Inserir anúncio'),
+        centerTitle: true,
       ),
       body: Column(children:[
           const Text('Nome do pet*',
+            textAlign: TextAlign.left,
             style: TextStyle(
               fontSize: 20,
             ),
-            textAlign: TextAlign.left,
           ),
           buildName(),
+
           const Text('Espécie*',
+            textAlign: TextAlign.left,
             style: TextStyle(
               fontSize: 20,
             ),
-            textAlign: TextAlign.left,
           ),
-          
-          const DropdownButtonExample(),
+          const DropdownButtonEspecie(),
+
+          const Text('Porte',
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          buildSize(),
+
+          const Text('Vacinas',
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          buildVaccines(),
         ]
       ),
       
--- lib/modules/home/ui/caixa_selecao.dart
+++ lib/modules/home/ui/caixa_selecao.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const List<String> list = <String>['Vira-Lata','Dog-Alemão','Bulldog','Vai se arrombar'];
+const List<String> list = <String>['Cachorro', 'Cavalo', 'Coelho', 'Gato', 'Jumento', 'Passáro', 'Porco', 'Rato'];
 
 class DropdownButtonApp extends StatelessWidget {
   const DropdownButtonApp({super.key});
@@ -11,21 +11,21 @@ class DropdownButtonApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(title: const Text('DropdownButton Sample')),
         body: const Center(
-          child: DropdownButtonExample(),
+          child: DropdownButtonEspecie(),
         ),
       ),
     );
   }
 }
 
-class DropdownButtonExample extends StatefulWidget {
-  const DropdownButtonExample({super.key});
+class DropdownButtonEspecie extends StatefulWidget {
+  const DropdownButtonEspecie({super.key});
 
   @override
-  State<DropdownButtonExample> createState() => _DropdownButtonExampleState();
+  State<DropdownButtonEspecie> createState() => _DropdownButtonEspecieState();
 }
 
-class _DropdownButtonExampleState extends State<DropdownButtonExample> {
+class _DropdownButtonEspecieState extends State<DropdownButtonEspecie> {
   String dropdownValue = list.first;
 
   @override
@@ -35,12 +35,14 @@ class _DropdownButtonExampleState extends State<DropdownButtonExample> {
       icon: const Icon(Icons.arrow_downward),
       elevation: 16,
       decoration: InputDecoration(
-        label: const Text('Espécie'),
         border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(6)
+          borderRadius: BorderRadius.circular(6),
         )
       ),
-      style: const TextStyle(color: Colors.blue),
+      style: const TextStyle(
+        color: Colors.grey,
+        fontSize: 20,
+        ),
       onChanged: (String? value) {
         // This is called when the user selects an item.
         setState(() {
