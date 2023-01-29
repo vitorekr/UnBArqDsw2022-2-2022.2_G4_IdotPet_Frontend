b7d34131f802d7d8b7f85e257ff1b3978d5b0d5b
jvsdurso <69814362+jvsdurso@users.noreply.github.com>
2023-01-28 22:10:05 -0300

    Adicionando descrição e CEP
    Co-Authored-By: Luan Cavalcante <67024690+Luan-Cavalcante@users.noreply.github.com>
    Co-Authored-By: vitorekr <56610229+vitorekr@users.noreply.github.com>
    Co-Authored-By: Nícolas Roberto <66215835+Nicolas-Roberto@users.noreply.github.com>

--- lib/modules/home/ui/cadastrar_pet.dart
+++ lib/modules/home/ui/cadastrar_pet.dart
@@ -137,6 +137,90 @@ Widget buildVaccines(){
   );
 }
 
+Widget buildDescription(){
+  return Column(
+    children: <Widget>[
+      Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.only(
+                left: 20,
+              ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              // ignore: prefer_const_literals_to_create_immutables
+              boxShadow: [
+                const BoxShadow(
+                  color: Colors.pink,
+                  blurRadius: 4,
+                  offset: Offset(0,2),
+                )
+              ]
+            ),
+            height: 60,
+            child: const TextField(
+                  maxLines: null,
+                  keyboardType: TextInputType.multiline,
+                  style: TextStyle(
+                    color: Colors.black38,
+                  ),
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    labelText: 'Descreva os detalhes do seu pet.\n',
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                    labelStyle: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
+                  ),
+                ),
+           ),
+        ),
+    ],
+  );
+}
+
+Widget buildCEP(){
+  return Column(
+    children: <Widget>[
+      Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.only(
+                left: 20,
+              ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              // ignore: prefer_const_literals_to_create_immutables
+              boxShadow: [
+                const BoxShadow(
+                  color: Colors.pink,
+                  blurRadius: 4,
+                  offset: Offset(0,2),
+                )
+              ]
+            ),
+            height: 60,
+            child: const TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(
+                    color: Colors.black38,
+                  ),
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.only(top:14),
+                    border: InputBorder.none,
+                    labelStyle: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
+                  ),
+                ),
+           ),
+        ),
+    ],
+  );
+}
+
 
 class CadastrarPet extends StatelessWidget {
   const CadastrarPet({super.key});
@@ -167,7 +251,7 @@ class CadastrarPet extends StatelessWidget {
           ),
           const DropdownButtonEspecie(),
 
-          const Text('Porte',
+          const Text('Porte*',
             textAlign: TextAlign.left,
             style: TextStyle(
               fontSize: 20,
@@ -175,13 +259,29 @@ class CadastrarPet extends StatelessWidget {
           ),
           buildSize(),
 
-          const Text('Vacinas',
+          const Text('Vacinas*',
             textAlign: TextAlign.left,
             style: TextStyle(
               fontSize: 20,
             ),
           ),
           buildVaccines(),
+
+          const Text('Descrição*',
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          buildDescription(),
+
+          const Text('CEP*',
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          buildCEP(),
         ]
       ),
       
