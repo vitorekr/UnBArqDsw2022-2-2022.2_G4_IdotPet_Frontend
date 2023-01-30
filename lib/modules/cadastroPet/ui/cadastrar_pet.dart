62cf71cd20f78caf3a9915d957e385f9e9cc04e1
Luan-Cavalcante <luanv.cavalcante@gmail.com>
2023-01-29 23:09:07 -0300

    Adicionando reusabilidade e botão.
    Co-Authored-By: João Durso <jvsdurso@gmail.com>
    Co-Authored-By: Vitor Kuhl <vitorekrlol@gmail.com>

--- lib/modules/cadastroPet/ui/cadastrar_pet.dart
+++ lib/modules/cadastroPet/ui/cadastrar_pet.dart
@@ -51,144 +51,6 @@ Widget buildPhotoUpload() {
   );
 }
 
-Widget buildName(){
-  return Column(
-    children: <Widget>[
-      Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.only(
-                left: 20,
-              ),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-              // ignore: prefer_const_literals_to_create_immutables
-              boxShadow: [
-                const BoxShadow(
-                  color: Colors.pink,
-                  blurRadius: 4,
-                  offset: Offset(0,2),
-                )
-              ]
-            ),
-            height: 60,
-            child: TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  // ignore: prefer_const_constructors
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 23
-                  ),
-                  // ignore: prefer_const_constructors
-                  decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.only(bottom:10),
-                    border: InputBorder.none,
-                    labelText: 'Ex.: Snoopy',
-                    floatingLabelBehavior: FloatingLabelBehavior.never,
-                    labelStyle: const TextStyle(
-                      color: Colors.black38,
-                      fontWeight: FontWeight.w400,
-                      fontSize: 20,
-                  ),
-                ),
-           ),
-        ),
-    ],
-  );
-}
-
-Widget buildSize(){
-  return Column(
-    children: <Widget>[
-      Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.only(
-                left: 20,
-              ),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-              // ignore: prefer_const_literals_to_create_immutables
-              boxShadow: [
-                const BoxShadow(
-                  color: Colors.pink,
-                  blurRadius: 4,
-                  offset: Offset(0,2),
-                )
-              ]
-            ),
-            height: 60,
-            child:  TextFormField(
-                  keyboardType: TextInputType.emailAddress, // E ESSE EMAILADRESS PRA TODO LADO?
-                  style: const TextStyle(                   // eh só arrumar
-                    color: Colors.black,
-                    fontSize: 25
-                  ),
-                  // ignore: prefer_const_constructors
-                  decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.only(bottom:10),
-
-                    border: InputBorder.none,
-                    labelText: 'Ex.: Médio',
-                    floatingLabelBehavior: FloatingLabelBehavior.never,
-                    labelStyle: const TextStyle(
-                    color: Colors.black38,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 20,
-                  ),
-                ),
-           ),
-        ),
-    ],
-  );
-}
-
-Widget buildVaccines(){
-  return Column(
-    children: <Widget>[
-      Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.only(
-                left: 20,
-              ),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-              // ignore: prefer_const_literals_to_create_immutables
-              boxShadow: [
-                const BoxShadow(
-                  color: Colors.pink,
-                  blurRadius: 4,
-                  offset: Offset(0,2),
-                )
-              ]
-            ),
-            height: 80,
-            child: const TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 23
-                  ),
-                  maxLines: 5,
-                  minLines: 1,
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.only(bottom:30),
-                    border: InputBorder.none,
-                    labelText: 'Descreva quais vacinas o animal já tomou. \n\nEx.: 4 V8s, 2 de Giardia, 1 de Raiva.',
-                    floatingLabelBehavior: FloatingLabelBehavior.never,
-                    labelStyle: TextStyle(
-                    color: Colors.black38,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 20,
-                    
-                  ),
-                ),
-           ),
-        ),
-    ],
-  );
-}
 
 Widget buildDescription(){
   return Column(
@@ -234,9 +96,22 @@ Widget buildDescription(){
   );
 }
 
-Widget buildCEP(){
+
+Widget buildGen(campo,descricao){
   return Column(
     children: <Widget>[
+      const SizedBox(
+        height: 15,
+      ),
+      Container(
+      alignment: Alignment.topLeft,
+      child: Text(campo,
+              textAlign: TextAlign.left,
+              style: const TextStyle(
+                fontSize: 20,
+              ),
+        ),
+      ),
       Container(
             alignment: Alignment.center,
             padding: const EdgeInsets.only(
@@ -255,16 +130,19 @@ Widget buildCEP(){
               ]
             ),
             height: 60,
-            child: const TextField(
+            
+            child: 
+            TextField(
                   keyboardType: TextInputType.emailAddress,
-                  style: TextStyle(
+                  style: const TextStyle(
                     color: Colors.black,
                     fontSize: 23
                   ),
                   decoration: InputDecoration(
+                    labelText: descricao,
                     contentPadding: EdgeInsets.only(bottom:10),
                     border: InputBorder.none,
-                    labelStyle: TextStyle(
+                    labelStyle: const TextStyle(
                     color: Colors.black38,
                     fontWeight: FontWeight.w400,
                     fontSize: 20,
@@ -272,10 +150,12 @@ Widget buildCEP(){
                 ),
            ),
         ),
+        const SizedBox(
+        height: 5,
+      ),
     ],
   );
 }
-
 
 class CadastrarPet extends StatelessWidget {
   const CadastrarPet({super.key});
@@ -297,22 +177,9 @@ class CadastrarPet extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children:[
             buildPhotoUpload(),
-            const SizedBox(
-              height: 15,
-            ),
-            const Text('Nome do pet*',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            buildName(),
-            const SizedBox(
-              height: 15,
-            ),
+            
+            buildGen("Nome do pet*","Ex. Jack Sparrow"),
+
             const Text('Espécie*',
               textAlign: TextAlign.left,
               style: TextStyle(
@@ -323,48 +190,12 @@ class CadastrarPet extends StatelessWidget {
               height: 5,
             ),
             const DropdownButtonEspecie(),
-            const SizedBox(
-              height: 15,
-            ),
-            const Text('Porte*',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            buildSize(),
-            const SizedBox(
-              height: 15,
-            ),
-            const Text('Vacinas*',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            buildVaccines(),
-            const SizedBox(
-              height: 15,
-            ),
-            const Text('CEP*',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            buildCEP(),
-            const SizedBox(
-              height: 15,
-            ),
+
+            buildGen("Raça*", "Ex. Pit Bull"),
+            buildGen("Porte*", "Ex. Médio"),
+            buildGen("Vacina*", "Descreva quais vacinas o animal já tomou. \n\nEx.: 4 V8s, 2 de Giardia, 1 de Raiva."),
+            buildGen("CEP*", "Ex. 73234600"),
+
             const Text('Descrição*',
               textAlign: TextAlign.left,
               style: TextStyle(
@@ -372,12 +203,13 @@ class CadastrarPet extends StatelessWidget {
               ),
             ),
             const SizedBox(
-              height: 5,
+              height: 10,
             ),
             buildDescription(),
             const SizedBox(
               height: 20,
             ),
+
             UnconstrainedBox(
                 child: Container(
                   height: 70,
@@ -407,13 +239,12 @@ class CadastrarPet extends StatelessWidget {
                     ),
                   ),
                 ),
-              )
+              ),
+              const SizedBox(height: 10,)
           ]
         ),
       ),
       bottomNavigationBar: const BottomNavBar(),
-
     );
   }
-
 }
